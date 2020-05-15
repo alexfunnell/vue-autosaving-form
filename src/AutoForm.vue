1e44185f9d991f9d0a0495f03790b9f1e6b09570
Alex Funnell <vulcno@icloud.com>
2020-05-15 16:59:36 +0100

    Create AutoForm.vue

---
+++ src/AutoForm.vue
@@ -0,0 +1,21 @@
+import { db } from './firebase';
+import { debounce } from 'debounce';
+
+const documentPath = 'contacts/jeff';
+
+export default {
+  data() {
+    return {
+      state: 'loading', // synced, modified, revoked, error
+      firebaseData: null,
+      formData: {},
+      errorMessage: ''
+    };
+  },
+
+  firestore() {
+    return {
+      firebaseData: db.doc(documentPath),
+    };
+  },
+});
